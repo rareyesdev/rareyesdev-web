d404b5804387689751eba667ad964bee16a01bcb
Ramón Alejandro Reyes Fajardo <rareyesdev@gmail.com>
2020-08-13 02:55:18 -0500

    Avoid throwing in production

--- src/utils/assertValue.ts
+++ src/utils/assertValue.ts
@@ -1,4 +1,4 @@
 export function assertValue<T>(value: T | null | undefined): value is T {
-  if (value !== null && value !== undefined) return true;
+  if ((value !== null && value !== undefined) || process.env.NODE_ENV === 'production') return true;
   throw new Error(`Failed value assertion. Value is ${String(value)}`);
 }
