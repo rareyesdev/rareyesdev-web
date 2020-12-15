91eae4460e4047e5abe5b013dee9ec28285a2814
Ramón Alejandro Reyes Fajardo <rareyesdev@gmail.com>
2020-12-15 10:18:42 -0600

    Add quote

--- src/components/RandomQuote/RandomQuote.tsx
+++ src/components/RandomQuote/RandomQuote.tsx
@@ -4,7 +4,7 @@ import Quote from '../Quote/Quote';
 import quotes from '../../assets/data/quotes.json';
 
 // Only some quotes will appear in the Intro section
-const introSectionQuotes = quotes.slice(0, 8);
+const introSectionQuotes = quotes.slice(0, 9);
 
 interface Props {
   rootClass?: string;
